1fe9b8fab584e86dc45e1d39ecb4976bba6cf66b
Kwibuka Elie <kebeann@192.168.1.118>
2026-04-26 21:21:45 +0200

    add getter method to PaymentRequest POJO

--- backend/Idempotency-gateway/src/main/java/com/finsafe/gateway/model/PaymentRequest.java
+++ backend/Idempotency-gateway/src/main/java/com/finsafe/gateway/model/PaymentRequest.java
@@ -4,13 +4,12 @@ public class PaymentRequest {
     private int amount;
     private String currency;
 
-    // Constructor
+   
     public PaymentRequest(int amount, String currency) {
         this.amount = amount;
         this.currency = currency;
     }
 
-    // Getters - These are what you were missing!
     public int getAmount() {
         return amount;
     }
